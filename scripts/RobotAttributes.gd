c74b20c89311bdf0417fef296093e9d051c786e9
loiveli <valtteri.nietula@gmail.com>
2026-03-05 15:04:50 +0200

    Updated robot attributes

--- scripts/RobotAttributes.gd
+++ scripts/RobotAttributes.gd
@@ -1,8 +1,8 @@
 class_name RobotAttributes
 extends Resource
 
-enum partType {NULL,ROBOT_LEG, ROBOT_ARM, ROBOT_HEAD, ROBOT_BODY}
-enum personalityTrait {OPEN_SOURCE, CLOSED_SOURCE, PROTOTYPE, EOL}
+enum partType {NULL,STEEL,ROBOT_LEG, ROBOT_ARM, ROBOT_HEAD, ROBOT_BODY, ROBOT_COMPLETE}
+enum personalityTrait {NULL, OPEN_SOURCE, CLOSED_SOURCE, PROTOTYPE, EOL}
 
 @export var name: String
 @export var outputSpeed: int
@@ -10,7 +10,7 @@ enum personalityTrait {OPEN_SOURCE, CLOSED_SOURCE, PROTOTYPE, EOL}
 @export var input: partType
 @export var personality: personalityTrait
 
-func _init(p_name = "", p_speed = 1, p_output = partType.ROBOT_LEG, p_input = partType.NULL, p_personality = personalityTrait.OPEN_SOURCE):
+func _init(p_name = "", p_speed = 1, p_output = partType.ROBOT_LEG, p_input = partType.NULL, p_personality = personalityTrait.NULL):
 	name = p_name
 	outputSpeed = p_speed
 	output = p_output
